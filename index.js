a0f9280c501ec3d1d14b7a7de61f753bd72a3e2d
snowdenfam <andre.snowden92@gmail.com>
2022-12-30 14:08:33 -0500

    Select all information , add to new object called "element"

--- index.js
+++ index.js
@@ -14,9 +14,35 @@ const fetchShelves = async () => {
  $('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20').each((_idx, el) => {
            const shelf = $(el)
            const title = shelf.find('span.a-size-base-plus.a-color-base.a-text-normal').text()
+
+           const image = shelf.find('img.s-image').attr('src')
+
+const link = shelf.find('a.a-link-normal.a-text-normal').attr('href')
+
+const reviews = shelf.find('div.a-section.a-spacing-none.a-spacing-top-micro > div.a-row.a-size-small').children('span').last().attr('aria-label')
+
+const stars = shelf.find('div.a-section.a-spacing-none.a-spacing-top-micro > div > span').attr('aria-label')
+
+const price = shelf.find('span.a-price > span.a-offscreen').text()
+
+
+    let element = {
+        title,
+        image,
+        link: `https://amazon.com${link}`,
+        price,
+    }
+
+    if (reviews) {
+        element.reviews = reviews
+    }
+
+    if (stars) {
+        element.stars = stars
+    }
            
 
-           shelves.push(title)
+           shelves.push(element)
        });
 
        return shelves;
